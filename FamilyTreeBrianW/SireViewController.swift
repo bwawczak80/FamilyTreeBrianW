fa8c3f207568f6ac960859c802226d7ba72f82af
Brian Wawczak <bwawczak@students.mchenry.edu>
2019-02-28 05:56:06 -0600

    functioning program

---
+++ FamilyTreeBrianW/SireViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SireViewController.swift
+//  FamilyTreeBrianW
+//
+//  Created by Brian Wawczak on 2/28/19.
+//  Copyright © 2019 Brian Wawczak. All rights reserved.
+//
+
+import UIKit
+
+class SireViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
